6e5d94786dae95b934bb26213cad94a756c4cf7f
Nathan Chan <nathanchan631@gmail.com>
2023-11-26 21:50:49 -0800

    Implement user matching with cosine similarity

--- src/app/app/swipe/page.jsx
+++ src/app/app/swipe/page.jsx
@@ -1,3 +1,12 @@
-export default function Swipe(){
+import "server-only";
+import getUser from "~/auth/getUser";
+import getMatches from "./matching"
+
+
+export default async function Swipe(){
+	const user = await getUser();
+	const similarities = await getMatches(user.auth.id);
+	console.log(similarities);
+
 	return <div>Swipe</div>
 }
