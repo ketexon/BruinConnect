46dfad78ef8b69ca06fdaa88b774526d3c1c86f0
ketexon <zane.a.s.clark@gmail.com>
2023-11-26 20:57:55 -0800

    fixed cookies in createBrowserClient

--- src/auth/createBrowserClient.js
+++ src/auth/createBrowserClient.js
@@ -8,5 +8,18 @@ export default function(){
 	return createClient(
 		process.env.NEXT_PUBLIC_SUPABASE_URL,
 		process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
+		{
+			auth: {
+				storage: {
+					setItem: (key, value) => document.cookie = `${encodeURIComponent(key)}=${encodeURIComponent(value)}`,
+					getItem: key => decodeURIComponent(document.cookie
+									.split("; ")
+									.find(line => line.startsWith(`${encodeURIComponent(key)}=`))
+									?.substring(key.length + 1)
+									?? null),
+					// removeItem: key => document.cookie = `${encodeURIComponent(key)}=; Max-Age=0`
+				}
+			}
+		}
 	)
 }
