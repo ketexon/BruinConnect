d2d31b776aabf6ec15ca82649f106be295eb7f28
ketexon <zane.a.s.clark@gmail.com>
2023-11-30 15:31:36 -0800

    added useSupabase hook, fixed getUser to select current user, added getOtherUser function

---
+++ src/auth/getOtherUser.js
@@ -0,0 +1,27 @@
+import * as React from "react";
+import { User as SupabaseUser, SupabaseClient } from "@supabase/supabase-js"
+import { User } from "./getUser"
+
+/**
+ * @param {SupabaseClient} supabase
+ * @param {string} userID
+ * @returns {Promis<Omit<"auth", User> | null>}
+ */
+export default async function(supabase, userID) {
+	const userData = await supabase
+		.from("Users")
+		.select("*")
+		.eq("UserUID", userID)
+		.maybeSingle();
+
+	const userImages = (await supabase
+		.from("UserImages")
+		.select("path")
+		.eq("user_id", userID))
+		.data;
+
+	return {
+		data: userData.data,
+		images: userImages.map(({ path }) => supabase.storage.from("images").getPublicUrl(path).data.publicUrl),
+	}
+}
