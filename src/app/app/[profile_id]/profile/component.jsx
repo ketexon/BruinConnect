e1a0680be260bdaa5549b47e3b0a0fea52916be0
ketexon <zane.a.s.clark@gmail.com>
2023-12-03 22:15:25 -0800

    made profile page loading happen on server

--- src/app/app/[profile_id]/profile/component.jsx
+++ src/app/app/[profile_id]/profile/component.jsx
@@ -4,8 +4,6 @@
 import React from 'react';
 import { Avatar, Typography, Box, Grid, Paper, IconButton } from '@mui/material';
 import Image from 'next/image'
-import ImageList from '@mui/material/ImageList';
-import ImageListItem from '@mui/material/ImageListItem';
 import EditIcon from '@mui/icons-material/Edit';
 import { useState, useEffect } from 'react';
 import MUIContainer, { ContainerProps } from "@mui/material/Container"
@@ -22,29 +20,19 @@ import useSupabase from '~/auth/useSupabase';
 // export const metadata = {
 //     title: 'BruinConnect | Profile',
 // }
-function ProfilePicture({ image }) {
+function ProfilePicture({ image, alt }) {
 	return (
 		<div style={{
+			width: "400px",
+			height: "250px",
 			display: "flex",
 			justifyContent: "center",
 		}}>
-			<Image src={image} className="circle" width="400" height="400"
-			/>
+			{image && <Image
+				src={image} className="circle" width="400" height="400"
+				alt={alt}
+			/>}
 		</div >
-	)
-}
-
-function EditButton() {
-	const [editing, setEditing] = useState(false);
-	const handleEditing = () => {
-		setEditing(true);
-	};
-
-	return (
-		<IconButton>
-			<EditIcon onClick={handleEditing} className="editbutton">
-			</EditIcon>
-		</IconButton>
 	)
 }
 
@@ -57,96 +45,57 @@ function DescriptionEditor({ initial_description, handleSave, readonly }) {
 			placeholder={placeholder}
 			defaultValue={initial_description}
 			onSave={(newDescription) => handleSave(newDescription)}
+			style={{
+				width: "100%"
+			}}
 		/>
 	);
 }
 
 
-export default function ({ user_id, profile_id }) {
+export default function ({ user_id, profile_id, user }) {
 	const supabase = useSupabase();
-	const [firstName, setFirstName] = useState();
-	const [lastName, setLastName] = useState();
-	const [description, setDescription] = useState();
-	const [profileImage, setProfileImage] = useState();
+	const firstName = user.data.FirstName;
+	const lastName = user.data.LastName;
+	const description = user.data.description;
+	const [profileImage, setProfileImage] = useState(
+		user.images.length > 0 ? user.images[0] : ""
+	);
 
 	function handleImageUpload(data) {
 		const new_image = supabase.storage.from('images').getPublicUrl(data).data.publicUrl;
 		setProfileImage(new_image)
-
 	}
 
-	// const handleDescriptionSave = (newValue) => {
-	// 	console.log("new value: " + newValue);
-	// 	supabase.from("Users").update({ description: newValue }).eq({ UserUID: profile_id })
-	// }
-
-	async function handleSave(newValue) {
-		console.log(profile_id + " " + newValue.value);
-
-		// supabase.from("Users").update({ description: newValue }).eq({ UserUID: profile_id });
-
+	async function handleSaveDescription(newValue) {
 		try {
-
 			const { data, error } = await supabase
 				.from('Users')
 				.update({ description: newValue.value })
 				.eq('UserUID', profile_id);
-			console.log(data)
-			console.log(error)
-
-			// const { error } = await supabase
-			// 	.from('Users')
-			// 	.insert({ description: newValue.value, UserUID: profile_id });
-
 		} catch (error) {
 			console.log(error);
 		}
 	}
 
-	useEffect(() => {
-		if (supabase === null) return;
-		const fetchData = async () => {
-			try {
-				const { data: data } = await supabase.from('Users').select('*').eq('UserUID', profile_id);
-				setFirstName(data[0].FirstName);
-				setLastName(data[0].LastName);
-				setDescription(data[0].description);
-
-				const { data: path } = await supabase.from('UserImages').select('path').eq('user_id', profile_id).limit(1);
-				const imageUrl = path.length ? supabase.storage.from("images").getPublicUrl(path[0].path).data.publicUrl : "";
-				setProfileImage(imageUrl);
-
-			} catch (error) {
-				console.log(error);
-			}
-		};
-
-		fetchData();
-	}, [supabase]);
-
 	return (
 		<>
 			<MUIContainer sx={{
 				position: 'relative',
-				height: '85vh', // Set the height to 50% of the viewport height
 				display: 'flex',
 				flexDirection: 'column',
 				alignItems: 'center',
 				justifyContent: 'center',
 			}}>
-
-				<ProfilePicture image={profileImage} />
+				<ProfilePicture image={profileImage} alt={`${firstName} ${lastName}'s profile photo`} />
 				{
 					user_id === profile_id && <ImageUpload onFileUpload={handleImageUpload} />
 				}
 				<h1>{firstName} {lastName} </h1>
-
-				<DescriptionEditor initial_description={description} handleSave={handleSave} readonly={user_id !== profile_id} />
-
+				<DescriptionEditor initial_description={description} handleSave={handleSaveDescription} readonly={user_id !== profile_id} />
 			</MUIContainer>
 
 		</>
 	);
 
 }
-
--- src/app/app/[profile_id]/profile/page.jsx
+++ src/app/app/[profile_id]/profile/page.jsx
@@ -2,13 +2,15 @@ import "server-only";
 import ProfilePage from "./component.jsx";
 import getUser from "~/auth/getUser";
 import createServerClient from "~/auth/createServerClient.js";
+import getOtherUser from "~/auth/getOtherUser.js";
 
 
 export default async function ({ params }) {
 	const supabase = createServerClient();
 	const user = await getUser(supabase);
+	const otherUser = await getOtherUser(supabase, params.profile_id);
 
 	return (
-		<ProfilePage user_id={user.auth.id} profile_id={params.profile_id}/>
+		<ProfilePage user={otherUser} editable={user.auth.id === params.profile_id} />
 	)
 }
